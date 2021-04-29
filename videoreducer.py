97b6158b3b999b3512df2d2fcce822a81f9937ef
Casey Ryan <caseyryan22465@gmail.com>
2021-04-29 00:17:16 -0400

    Began HW encoding, program works but want to make it better

--- videoreducer.py
+++ videoreducer.py
@@ -1,11 +1,13 @@
 import subprocess
 import os, sys, getopt, time
+import psutil
 
 #https://stackoverflow.com/questions/9319317/quick-and-easy-file-dialog-in-python
 #add gui and make it a real not command line project?
 
 def main():
     args = sys.argv[1:]
+
     path = os.path.join(args[0])
     preset = args[1]
     if(len(args) == 3):
@@ -13,52 +15,67 @@ def main():
     else:
         pmax = 5
     hevc = True
+
     startReduceTime = time.time()
+
     if(os.path.isdir(path)):
         print("Beginning at " + time.strftime("%H:%M:%S", time.localtime()))
-        statuses = reduceDir(path, True, 'fast', pmax)
+        statuses = reduceDir(path, True, preset, pmax)
     else:
-        print("Need directory")
+        print("Invalid directory")
         return -1
     print(statuses)
     print("Finished " + str(len(statuses)) + " reductions in " + str(time.time()-startReduceTime)+ "s")
     
 def reduceDir(path, hevc=True, preset='fast', pmax=5):
+    #https://stackoverflow.com/questions/46849574/start-process-with-low-priority-popen
+    #set process priority to low so subprocess ffmpeg processes start as low.
+    psutil.Process().nice(psutil.BELOW_NORMAL_PRIORITY_CLASS)
+
     processes = []
     codes = []
+    hw = False
     for file in os.listdir(path):
             if(file.endswith(".mp4")):
                 #start = time.strftime("%H:%M:%S" ,time.localtime())
                 #print("starting conversion for: " + file + ", time started: " + start)
                 print("Beginning " + file)
-                processes.append([file, subprocess.Popen(['ffmpeg','-y','-hide_banner', '-loglevel', 'error', '-i', os.path.abspath(os.path.join(path, file)), '-c:v','libx265', '-x265-params', 'log-level=error', '-preset', preset, '-c:a', 'copy', os.path.abspath(os.path.join(path + '\out', file[0:file.rfind('.')])) + '-reduced_' + preset + '.mp4'])])
-    
+                outLevel = 'error'
+                outputLevel = ['-loglevel', outLevel]
+                inputArgs = ['-i', os.path.abspath(os.path.join(path, file))]
+                codecArgs = ['-c:v', 'hevc_amf', '-c:a', 'copy'] if (hevc and hw) else ['-c:v', 'libx265', '-preset', preset, '-x265-params', 'log-level=error', '-c:a', 'copy']
+                outputArgs = [os.path.abspath(os.path.join(path + '\out', file[0:file.rfind('.')])) + '-reduced_' + preset + '.mp4']
+                argslist = [
+                    'ffmpeg',
+                    '-y',
+                    '-hide_banner',
+                    *outputLevel,
+                    *inputArgs,
+                    *codecArgs,
+                    *outputArgs
+                    ]
+                processes.append([file, subprocess.Popen(argslist), time.time()])
+
+                #had lines 65-69 up here too to not only check for when len(p) > pmax, but all the time. However I don't know if checking all processes for finish every time a new one is created is useful or cheaper than just waiting til needed.
+
+            #could use multithreading to constantly check for finished processes, only checking when processes>pmax might be wasteful/bottleneck
             while(len(processes) >= pmax):
-                #waits for the shortest one every time. should make object that instead stores name, path, popen, time started, and size. Wait strategically (some function of time started and size)
                 for p in processes:
                     if(p[1].poll() == 0):
                         codes.append(p[1].returncode)
-                        print("conversion " + p[0] + " exited")
+                        print(p[0] + " exited after " + str(time.time() - p[2])[:-13] + 's')
                         processes.remove(p)
-    
-                minSize = os.path.getsize(path + '\\' + processes[0][0])
-                waitTarget = processes[0]
-                for p in processes:
-                    if(os.path.getsize(path + '\\' + p[0]) < minSize):
-                        waitTarget = p
-                #print(str(len(processes)) + " processes greater than limit "+ str(pmax) + ", waiting for " + p[0] + " to finish")
-                #try:
-                #print(waitTarget)
-                codes.append(waitTarget[1].wait())
-                #except:
-                print("finished " + waitTarget[0])
-                processes.remove(waitTarget)
-                #print(str(len(processes)) + " processes active")
-                
-    for p in processes:
-        codes.append(p[1].wait())
-        print("finished " + p[0])
+                #reduce busy waiting?
+                time.sleep(.1)
 
+    #program gets here very early on. wasteful
+    while(len(processes) > 0):
+        for p in processes:
+            if(p[1].poll() == 0):
+                codes.append(p[1].returncode)
+                print(p[0] + " exited after " + str(time.time() - p[2])[:-13] + 's')
+                processes.remove(p)
+        time.sleep(.1)
     return codes
 
 #def reduceVideo(filePath, outName, hevc = True, preset='slow'):
